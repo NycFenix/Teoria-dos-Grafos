505afe539da277f1a6302b69b845856a8d7e7a6e
srtapacheco <karenpacheco16@poli.ufrj.br>
2023-09-19 07:47:15 -0300

    Restruturando funcionalidades principais de calculo das informacoes basicas do grafo e alterando descricao do projeto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Teoria-dos-Grafos
+# Teoria dos Grafos - COS 242 (2023/2) - Trabalho de Disciplina - Parte 
+
+Este repositório contém os materiais e o código relacionados ao Trabalho de Disciplina - Parte 1 da disciplina de Teoria dos Grafos (COS 242) no segundo semestre de 2023.
+
+## Objetivos
+O objetivo deste trabalho é projetar e desenvolver uma biblioteca para manipular grafos. A biblioteca deve ser capaz de representar grafos e implementar um conjunto de algoritmos em grafos. A biblioteca deve ser projetada de forma a ser facilmente utilizada em outros programas.
+
+## Contribuição
+Este é um trabalho de disciplina e não aceita contribuições externas neste repositório. No entanto, fique à vontade para utilizar este código como referência para projetos futuros.
--- bibliotecaGrafos.py
+++ bibliotecaGrafos.py
@@ -1,66 +1,71 @@
-import numpy as np
+class Grafo:
+    def __init__(self):
+        self.vertices = set()
+        self.arestas = []
 
-texto = open("entrada.txt", "r")
-    
-vertice = int(texto.readline())
-linhas = texto.readlines()
-grafo= []
+    def adicionar_aresta(self, u, v):
+        self.vertices.add(u)
+        self.vertices.add(v)
+        self.arestas.append((u, v))
 
-for linha in linhas[0:]:
-    grafo.append(linha.strip().split())
+    def calcular_informacoes(self):
+        num_vertices = len(self.vertices)
+        num_arestas = len(self.arestas)
+        graus = [0] * num_vertices
 
-print("vertice:",vertice)
-print("arestas:", grafo)
-texto.close()
+        for u, v in self.arestas:
+            graus[u - 1] += 1
+            graus[v - 1] += 1
 
-def recorrencia(verticeProcurado):
-    recorrencia = 0
-    for linha in grafo:
-        for item in linha:
-            if item == verticeProcurado:
-                recorrencia += 1
+        grau_minimo = min(graus)
+        grau_maximo = max(graus)
+        grau_medio = sum(graus) / num_vertices
 
-    return recorrencia
+        graus.sort()
+        mediana = graus[num_vertices // 2]
 
-    print("recorrencia do vertice:", recorrencia("3"))
+        return num_vertices, num_arestas, grau_minimo, grau_maximo, grau_medio, mediana
 
+    def componentes_conexas(self):
+        # Implementar aqui o algoritmo BFS ou DFS para encontrar as componentes conexas.
+        pass
 
-
-# class Grafo:
-#     def __init__(self, vertices, arestas):
-#          self.vertices = vertices
-#          self.arestas = arestas
-      
-        
-        
-    
-#     def vetorDeAjacencia(self):
-#         vetor = np.array([])
-#         for vertice in range (1, self.vertices):
-#             vetor.concatenate(vetor)
-#             for linha in self.arestas:
-#                 if str(vertice) in linha:
-                    
-         
-#         for linha in self.arestas:
-
-        
-#     def getGrauMinimo(self):
-        
-#         for linha in self.arestas:
+#    def vetorDeAjacencia(self):
+#        vetor = np.array([])
+#        for vertice in range (1, self.vertices):
+#            vetor.concatenate(vetor)
+#            for linha in self.arestas:
+#                if str(vertice) in linha:
+#        for linha in self.arestas:
             
-#     def getGrauMaximo(self):
-        
-#     def getGrauMedio(self):
-    
-#     def getMedianaDeGrau(self):
-            
-#     def getGrauMaximo(self):
-        
-#     def getGrauMedio(self):
-    
-#     def getMedianaDeGrau(self):
-        
+
+    def ler_grafo(self, arquivo_entrada):
+        with open(arquivo_entrada, 'r') as arquivo:
+            num_vertices = int(arquivo.readline().strip())
+            for linha in arquivo:
+                u, v = map(int, linha.split())
+                self.adicionar_aresta(u, v)
+
+    def escrever_informacoes(self, arquivo_saida):
+        num_vertices, num_arestas, grau_minimo, grau_maximo, grau_medio, mediana = self.calcular_informacoes()
+
+        with open(arquivo_saida, 'w', encoding='utf-8') as arquivo:
+            arquivo.write(f"Número de vértices: {num_vertices}\n")
+            arquivo.write(f"Número de arestas: {num_arestas}\n")
+            arquivo.write(f"Grau mínimo: {grau_minimo}\n")
+            arquivo.write(f"Grau máximo: {grau_maximo}\n")
+            arquivo.write(f"Grau médio: {grau_medio:.2f}\n")
+            arquivo.write(f"Mediana de grau: {mediana}\n")
+
+
+if __name__ == "__main__":
+    meu_grafo = Grafo()
+    meu_grafo.ler_grafo("entrada.txt")
+    meu_grafo.componentes_conexas() 
+    meu_grafo.escrever_informacoes("informacoes.txt")
+
+
+
           
          
             
